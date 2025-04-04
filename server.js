2639b1b614d80f8100214c4fd78be319dea59b2c
David Robin <davidro@jfrog.com>
2025-04-04 15:04:54 +0200

    add vuln exploitable

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-Object.freeze(Object.prototype);
+//Object.freeze(Object.prototype);
 var express = require('express');
 const bodyParser = require('body-parser');
 const lodash = require('lodash'); 
